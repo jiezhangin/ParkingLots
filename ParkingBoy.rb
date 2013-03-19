099c1d3d86e753fb1f00bbe473fb5a4b970e1ed2
ZJ <jiezhang.in@gmail.com>
2013-03-19 15:28:17 +0800

    could park car and pick car by ticket

--- ParkingBoy.rb
+++ ParkingBoy.rb
@@ -1,15 +1,38 @@
 class ParkingBoy
-	def initialize(numOfParkingLots)
-		@numOfParkingLots = numOfParkingLots
+	def initialize(parkingLotsList)
+		@numOfParkingLots = parkingLotsList.length
+		@parkingLotsList = parkingLotsList
 		
 	end
 	
 	attr_reader :numOfParkingLots
 	
 	def has_empty
-		if @numOfParkingLots > 0
-			return true
+		 @parkingLotsList.each do |parkingLots|
+			if !parkingLots.empty?
+				return true
+			end
 		end
 		return false
+	end
+	
+	def park(carID)
+		@parkingLotsList.each do |parkingLots|
+			if !parkingLots.empty?
+				return parkingLots.park(carID)
+			end
+		end
+		raise NoParkingLotsException.new("Sorry, no parking lot avaialbe!")
+	end
+	
+	def pick(ticket)
+		@parkingLotsList.each do |parkingLots|
+			car = parkingLots.pick(ticket)
+			if car
+				puts "car is found"
+				return car
+			end
+		end
+		return nil
 	end
 end
